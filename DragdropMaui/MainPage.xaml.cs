fe6e750e65685e765a3ee4a7f2dfc176534f8f51
hobbelb <hobbelb@VT-LT-221>
2022-03-09 15:51:09 +0100

    Add project files.

---
+++ DragdropMaui/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace DragdropMaui;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+   
+
+    private void DragGestureRecognizer_DragStarting_1(object sender, DragStartingEventArgs e)
+    {
+        var label = (sender as Element)?.Parent as Label;
+        e.Data.Properties.Add("Text", label.Text);
+    }
+
+    private void DropGestureRecognizer_Drop_1(object sender, DropEventArgs e)
+    {
+        var data = e.Data.Properties["Text"].ToString();
+        var frame = (sender as Element)?.Parent as Frame;
+        frame.Content = new Label
+        {
+            Text = data
+        };
+    }
+}
+
